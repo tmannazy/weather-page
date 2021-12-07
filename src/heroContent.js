4fe594099ac810c5465ce4e62c71ad4b781eecf1
tmannazy <e.anene@hotmail.com>
2021-12-07 21:43:17 +0100

    Create a function to build fetched weather data

--- src/heroContent.js
+++ src/heroContent.js
@@ -1,55 +1,81 @@
-const showDefaultWeather = () => {
-  const weatherContent = [
-    "date",
-    "temp",
-    "humidity",
-    "wind_speed",
-    "feels_like",
-  ];
-  const displayWeatherRetrieved = document.createElement("div");
-  const weatherPageData = document.createElement("div");
-  const weatherPageInfo = document.createElement("div");
+const weatherContent = ["date", "temp", "humidity", "wind_speed", "feels_like"];
+const displayWeatherRetrieved = document.createElement("div");
+const weatherPageData = document.createElement("div");
+const weatherPageInfo = document.createElement("div");
 
+displayWeatherRetrieved.className = "weather-container";
+
+const showDefaultWeather = () => {
   document.addEventListener("DOMContentLoaded", () => {
     async function displayWeather() {
       const weatherData = await fetch(
-        "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=metric&appid=42cb9ecb74688a62504925b13afb6382",
+        "https://api.openweathermap.org/data/2.5/onecall?lat=51.51&lon=-0.13&units=metric&appid=42cb9ecb74688a62504925b13afb6382",
         { mode: "cors" }
       );
       const storeWeatherData = await weatherData.json();
       //   const showTemperature = await storeWeatherData.main.temp;
       // console.log(showTemperature);
       console.log(storeWeatherData);
-      const showContentOfWeather = weatherContent.forEach((item) => {
-        const containerDivForAll = document.createElement("div");
-        const options = {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        };
-        const dateInTimestamp = new Date(storeWeatherData.current.dt * 1000);
-        const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
-          dateInTimestamp
-        );
-        switch (item) {
-          case "date":
-            containerDivForAll.textContent = formattedDate;
-            containerDivForAll.className = `weather-${item}-container`;
-            weatherPageData.appendChild(containerDivForAll);
-            break;
-          case "temp":
-            containerDivForAll.textContent = `${storeWeatherData.current.temp}\xB0C`;
-            containerDivForAll.className = `weather-${item}-container`;
-            weatherPageInfo.appendChild(containerDivForAll);
-            break;
-        }
-      });
+      //   const showContentOfWeather = weatherContent.forEach((item) => {
+      //     const containerDivForAll = document.createElement("div");
+      //     const options = {
+      //       weekday: "long",
+      //       year: "numeric",
+      //       month: "long",
+      //       day: "numeric",
+      //     };
+      //     const dateInTimestamp = new Date(storeWeatherData.current.dt * 1000);
+      //     const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
+      //       dateInTimestamp
+      //     );
+      //     switch (item) {
+      //       case "date":
+      //         containerDivForAll.textContent = formattedDate;
+      //         containerDivForAll.className = `weather-${item}-container`;
+      //         weatherPageData.appendChild(containerDivForAll);
+      //         break;
+      //       case "temp":
+      //         containerDivForAll.textContent = `${storeWeatherData.current.temp}\xB0C`;
+      //         containerDivForAll.className = `weather-${item}-container`;
+      //         weatherPageInfo.appendChild(containerDivForAll);
+      //   const getInfoOfWeather = showContentOfWeather(storeWeatherData);
+      //   displayWeatherRetrieved.append(showContentOfWeather(storeWeatherData));
+      //   return getInfoOfWeather;
+      //   return displayWeatherRetrieved;
     }
     displayWeather();
   });
-  displayWeatherRetrieved.append(weatherPageData, weatherPageInfo);
+  //   displayWeatherRetrieved.append(weatherPageData, weatherPageInfo);
+};
+
+const showContentOfWeather = (fetchedWeather) => {
+  weatherContent.forEach((item) => {
+    const containerDivForAll = document.createElement("div");
+    containerDivForAll.textContent = "";
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const dateInTimestamp = new Date(fetchedWeather.current.dt * 1000);
+    const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
+      dateInTimestamp
+    );
+    switch (item) {
+      case "date":
+        containerDivForAll.textContent = formattedDate;
+        containerDivForAll.className = `weather-${item}-container`;
+        displayWeatherRetrieved.appendChild(containerDivForAll);
+        break;
+      case "temp":
+        containerDivForAll.textContent = `${fetchedWeather.current.temp}\xB0C`;
+        containerDivForAll.className = `weather-${item}-container`;
+        displayWeatherRetrieved.appendChild(containerDivForAll);
+        break;
+    }
+  });
   return displayWeatherRetrieved;
 };
 
-export { showDefaultWeather };
+export { showDefaultWeather, showContentOfWeather };
