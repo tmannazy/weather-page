be21962df3a76e4f2fddd6614188f73ddbaf0b59
tmannazy <e.anene@hotmail.com>
2021-12-08 12:15:12 +0100

    Empty the displayWeatherRetrieved div for new data

--- src/heroContent.js
+++ src/heroContent.js
@@ -1,57 +1,55 @@
 const weatherContent = ["date", "temp", "humidity", "wind_speed", "feels_like"];
 const displayWeatherRetrieved = document.createElement("div");
 const weatherPageData = document.createElement("div");
-const weatherPageInfo = document.createElement("div");
+// const displayWeatherRetrieved = document.createElement("div");
 
 displayWeatherRetrieved.className = "weather-container";
 
 const showDefaultWeather = () => {
+  displayWeatherRetrieved.textContent = "";
   document.addEventListener("DOMContentLoaded", () => {
-    async function displayWeather() {
+    async function displayDefaultWeather() {
       const weatherData = await fetch(
         "https://api.openweathermap.org/data/2.5/onecall?lat=51.51&lon=-0.13&units=metric&appid=42cb9ecb74688a62504925b13afb6382",
         { mode: "cors" }
       );
       const storeWeatherData = await weatherData.json();
-      //   const showTemperature = await storeWeatherData.main.temp;
-      // console.log(showTemperature);
       console.log(storeWeatherData);
-      //   const showContentOfWeather = weatherContent.forEach((item) => {
-      //     const containerDivForAll = document.createElement("div");
-      //     const options = {
-      //       weekday: "long",
-      //       year: "numeric",
-      //       month: "long",
-      //       day: "numeric",
-      //     };
-      //     const dateInTimestamp = new Date(storeWeatherData.current.dt * 1000);
-      //     const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
-      //       dateInTimestamp
-      //     );
-      //     switch (item) {
-      //       case "date":
-      //         containerDivForAll.textContent = formattedDate;
-      //         containerDivForAll.className = `weather-${item}-container`;
-      //         weatherPageData.appendChild(containerDivForAll);
-      //         break;
-      //       case "temp":
-      //         containerDivForAll.textContent = `${storeWeatherData.current.temp}\xB0C`;
-      //         containerDivForAll.className = `weather-${item}-container`;
-      //         weatherPageInfo.appendChild(containerDivForAll);
-      //   const getInfoOfWeather = showContentOfWeather(storeWeatherData);
-      //   displayWeatherRetrieved.append(showContentOfWeather(storeWeatherData));
-      //   return getInfoOfWeather;
-      //   return displayWeatherRetrieved;
+      const showContentOfWeather = weatherContent.forEach((item) => {
+        const containerDivForAll = document.createElement("div");
+        const options = {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+        const dateInTimestamp = new Date(storeWeatherData.current.dt * 1000);
+        const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
+          dateInTimestamp
+        );
+        switch (item) {
+          case "date":
+            containerDivForAll.textContent = formattedDate;
+            containerDivForAll.className = `weather-${item}-container`;
+            displayWeatherRetrieved.appendChild(containerDivForAll);
+            break;
+          case "temp":
+            containerDivForAll.textContent = `${storeWeatherData.current.temp}\xB0C`;
+            containerDivForAll.className = `weather-${item}-container`;
+            displayWeatherRetrieved.appendChild(containerDivForAll);
+            break;
+        }
+      });
+      return displayWeatherRetrieved;
     }
-    displayWeather();
+    displayDefaultWeather();
   });
-  //   displayWeatherRetrieved.append(weatherPageData, weatherPageInfo);
 };
 
 const showContentOfWeather = (fetchedWeather) => {
+  displayWeatherRetrieved.textContent = "";
   weatherContent.forEach((item) => {
     const containerDivForAll = document.createElement("div");
-    containerDivForAll.textContent = "";
     const options = {
       weekday: "long",
       year: "numeric",
