40076341bd110090f672f8b0105fffcd317b517e
tmannazy <e.anene@hotmail.com>
2021-12-14 01:18:14 +0100

    Display error message if location is not found

--- src/index.js
+++ src/index.js
@@ -5,35 +5,45 @@ const content = document.querySelector(".content");
 const queryLocation = document.createElement("input");
 const searchBtn = document.createElement("button");
 const defaultWeatherContainer = document.createElement("section");
+const errorContainer = document.createElement("div");
 
 queryLocation.setAttribute("type", "text");
 queryLocation.setAttribute("class", "search");
+errorContainer.setAttribute("class", "error-info");
 searchBtn.textContent = "Search";
 defaultWeatherContainer.append(showDefaultWeather());
 content.append(pageHeader(), queryLocation, searchBtn, defaultWeatherContainer);
 
 searchBtn.addEventListener("click", () => {
   const useVal = queryLocation.value.toLocaleLowerCase();
-
   async function displayWeather() {
-    const weatherData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${useVal}&units=imperial&appid=42cb9ecb74688a62504925b13afb6382`
-    );
-    const getWeatherData = await weatherData.json();
-    const longitude = getWeatherData.coord.lon;
-    const latitude = getWeatherData.coord.lat;
-    const getUserWeatherQuery = await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=42cb9ecb74688a62504925b13afb6382`,
-      { mode: "cors" }
-    );
-    const parseFetchedUserWeatherQuery = await getUserWeatherQuery.json();
-    defaultWeatherContainer.textContent = "";
-    defaultWeatherContainer.append(
-      showContentOfWeather(parseFetchedUserWeatherQuery)
-    );
+    try {
+      const weatherData = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${useVal}&units=imperial&appid=42cb9ecb74688a62504925b13afb6382`
+      );
+      const getWeatherData = await weatherData.json();
+      if (getWeatherData.cod === 200) {
+        useWeather(getWeatherData);
+      } else {
+        errorContainer.textContent = `${getWeatherData.message}. Try again later.`;
+        defaultWeatherContainer.appendChild(errorContainer);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
-
+  // async function useWeather(weatherDataReceived) {
+  //   const longitude = getWeatherData.coord.lon;
+  //   const latitude = getWeatherData.coord.lat;
+  //   const getUserWeatherQuery = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=42cb9ecb74688a62504925b13afb6382`,
+  //     { mode: "cors" }
+  //   );
+  //   const parseFetchedUserWeatherQuery = await getUserWeatherQuery.json();
+  //   defaultWeatherContainer.textContent = "";
+  //   defaultWeatherContainer.append(
+  //     showContentOfWeather(parseFetchedUserWeatherQuery)
+  //   );
+  // }
   displayWeather();
 });
-
-// console.log(firPromise());
