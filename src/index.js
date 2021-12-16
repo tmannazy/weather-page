79e2452efc2d16b118dd6193a6528abea75c523c
tmannazy <e.anene@hotmail.com>
2021-12-16 21:45:59 +0100

    Refactor code to display more temperature data

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const searchBtn = document.createElement("button");
 const defaultWeatherContainer = document.createElement("section");
 const errorContainer = document.createElement("div");
 const temperatureBtn = document.createElement("button");
-let weatherDataReceivedInFahrenheit;
-let weatherDataReceivedInCelsius;
+let fetchedDataInFahrenheit;
+let fetchedDataInCelsius;
 
 queryLocation.setAttribute("type", "text");
 queryLocation.setAttribute("class", "search");
@@ -27,15 +27,13 @@ content.append(
 
 searchBtn.addEventListener("click", () => {
   const userQuery = queryLocation.value.toLocaleLowerCase();
-  let fetchedLocationName;
   async function displayWeather() {
     const weatherData = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${userQuery}&units=imperial&appid=42cb9ecb74688a62504925b13afb6382`,
       { mode: "cors" }
     );
     const getWeatherData = await weatherData.json();
-    fetchedLocationName = getWeatherData.name;
-    weatherDataReceivedInFahrenheit = getWeatherData;
+    fetchedDataInFahrenheit = getWeatherData;
     return getWeatherData;
   }
 
@@ -49,10 +47,13 @@ searchBtn.addEventListener("click", () => {
         { mode: "cors" }
       );
       const parseFetchedUserWeatherQuery = await getUserWeatherQuery.json();
-      weatherDataReceivedInCelsius = parseFetchedUserWeatherQuery;
+      fetchedDataInCelsius = parseFetchedUserWeatherQuery;
       defaultWeatherContainer.textContent = "";
       defaultWeatherContainer.append(
-        showContentOfWeather(parseFetchedUserWeatherQuery, fetchedLocationName)
+        showContentOfWeather(
+          parseFetchedUserWeatherQuery,
+          fetchedDataInFahrenheit
+        )
       );
     } catch (error) {
       if (defaultWeatherContainer.textContent.match(/\bnetwork error\b/i)) {
@@ -71,16 +72,25 @@ searchBtn.addEventListener("click", () => {
 
 temperatureBtn.addEventListener("click", (e) => {
   const getTempContainer = document.querySelector(".weather-temp-container");
+  const getFeelsLikeContainer = document.querySelector(
+    ".weather-feels_like-container"
+  );
 
   if (e.target.textContent === "Display °F") {
     e.target.textContent = "Display \xB0C";
     getTempContainer.textContent = `${Math.ceil(
-      weatherDataReceivedInFahrenheit.main.temp
+      fetchedDataInFahrenheit.main.temp
+    )} \xB0F`;
+    getFeelsLikeContainer.textContent = `${Math.ceil(
+      fetchedDataInFahrenheit.main.feels_like
     )} \xB0F`;
   } else {
     e.target.textContent = "Display \xB0F";
     getTempContainer.textContent = `${Math.ceil(
-      weatherDataReceivedInCelsius.current.temp
+      fetchedDataInCelsius.current.temp
+    )} \xB0C`;
+    getFeelsLikeContainer.textContent = `${Math.ceil(
+      fetchedDataInCelsius.current.feels_like
     )} \xB0C`;
   }
 });
