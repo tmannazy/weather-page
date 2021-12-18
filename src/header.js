b24484f8b0cf342bd56498ea5ce16b7d1fbbb9d9
tmannazy <e.anene@hotmail.com>
2021-12-18 11:44:14 +0100

    Add a class to img and img src url

--- src/header.js
+++ src/header.js
@@ -3,7 +3,8 @@ const pageHeader = () => {
   const pageIcon = document.createElement("img");
   const h1 = document.createElement("h1");
 
-  pageIcon.setAttribute("src", "");
+  pageIcon.setAttribute("src", "../dist/87f7f47e922b5771362f.jpg");
+  pageIcon.classList.add("page-icon");
   h1.textContent = "Weather Page";
   h1.classList.add("page-title");
   header.append(pageIcon, h1);
